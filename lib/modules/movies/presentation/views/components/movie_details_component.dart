85c8afd254ab61a47f2e2a1811611b99955a85e3
Mohamed hany <107276176+mohamedhany7420@users.noreply.github.com>
2025-01-30 03:54:30 +0200

    handling img url error in movie details

---
+++ lib/modules/movies/presentation/views/components/movie_details_component.dart
@@ -0,0 +1,205 @@
+
+import 'package:animate_do/animate_do.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../../core/utils/enums.dart';
+import '../../../domain/entities/genres.dart';
+import '../../controller/movie_details_bloc.dart';
+import '../../controller/movie_details_state.dart';
+import '../widgets/custom_movie_image.dart';
+import '../widgets/custom_recommendation_item.dart';
+
+class MovieDetailsContent extends StatelessWidget {
+
+  const MovieDetailsContent({
+    super.key,
+  });
+  @override
+  Widget build(BuildContext context) {
+    return BlocBuilder<MovieDetailsBloc, MovieDetailsState>(
+      builder: (context, state) {
+        if (state.movieDetailsState case RequestStates.loaded) {
+          return CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                pinned: true,
+                expandedHeight: 200.0,
+                flexibleSpace: FlexibleSpaceBar(
+                  background: CustomMovieImage(
+                    image: state.movieDetails?.backdropPath??'https://abtc.ng/wp-content/uploads/2023/10/Palestine44.webp',
+                  ),
+                ),
+              ),
+              SliverToBoxAdapter(
+                child: FadeInUp(
+                  from: 20,
+                  duration: const Duration(milliseconds: 500),
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(state.movieDetails?.title??'Unknown',
+                            style: GoogleFonts.poppins(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w700,
+                              letterSpacing: 1.2,
+                            )),
+                        const SizedBox(height: 8.0),
+                        Row(
+                          children: [
+                            Container(
+                              padding: const EdgeInsets.symmetric(
+                                vertical: 2.0,
+                                horizontal: 8.0,
+                              ),
+                              decoration: BoxDecoration(
+                                color: Colors.grey[800],
+                                borderRadius: BorderRadius.circular(4.0),
+                              ),
+                              child: Text(
+                                state.movieDetails?.releaseDate?.split('-')[0]??'unknown',
+                                style: const TextStyle(
+                                  fontSize: 16.0,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ),
+                            const SizedBox(width: 16.0),
+                            Row(
+                              children: [
+                                const Icon(
+                                  Icons.star,
+                                  color: Colors.amber,
+                                  size: 20.0,
+                                ),
+                                const SizedBox(width: 4.0),
+                                Text(
+                                  (state.movieDetails?.voteAverage?? 3 / 2).toStringAsFixed(1),
+                                  style: const TextStyle(
+                                    fontSize: 16.0,
+                                    fontWeight: FontWeight.w500,
+                                    letterSpacing: 1.2,
+                                  ),
+                                ),
+                                const SizedBox(width: 4.0),
+                                Text(
+                                  '(${state.movieDetails?.voteAverage})',
+                                  style: const TextStyle(
+                                    fontSize: 1.0,
+                                    fontWeight: FontWeight.w500,
+                                    letterSpacing: 1.2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            const SizedBox(width: 16.0),
+                            Text(
+                              _showDuration(state.movieDetails?.runTime?? 160),
+                              style: const TextStyle(
+                                color: Colors.white70,
+                                fontSize: 16.0,
+                                fontWeight: FontWeight.w500,
+                                letterSpacing: 1.2,
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20.0),
+                        Text(
+                          state.movieDetails?.overview??'unknown',
+                          style: const TextStyle(
+                            fontSize: 14.0,
+                            fontWeight: FontWeight.w400,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                        const SizedBox(height: 8.0),
+                        Text(
+                          'Genres: ${_showGenres(state.movieDetails?.genres??[])}',
+                          style: const TextStyle(
+                            color: Colors.white70,
+                            fontSize: 12.0,
+                            fontWeight: FontWeight.w500,
+                            letterSpacing: 1.2,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              SliverPadding(
+                padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 24.0),
+                sliver: SliverToBoxAdapter(
+                  child: FadeInUp(
+                    from: 20,
+                    duration: const Duration(milliseconds: 500),
+                    child: Text(
+                      'More like this'.toUpperCase(),
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.w500,
+                        letterSpacing: 1.2,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              // Tab(text: 'More like this'.toUpperCase()),
+              SliverPadding(
+                padding: const EdgeInsets.fromLTRB(16.0, 0.0, 16.0, 24.0),
+                sliver:  SliverGrid(
+                  delegate: SliverChildBuilderDelegate(
+                        (context, index) {
+                      final recommendation = state.recommendations[index];
+                      return CustomRecommendationItem(recommendation: recommendation);
+                    },
+                    childCount: state.recommendations.length,
+                  ),
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    mainAxisSpacing: 8.0,
+                    crossAxisSpacing: 8.0,
+                    childAspectRatio: 0.7,
+                    crossAxisCount: 3,
+                  ),
+                ),
+              ),
+            ],
+          );
+        } else {
+          return Center(child: Text(state.movieDetailsErrMessage));
+        }
+      },
+    );
+  }
+
+  String _showGenres(List<Genres> genres) {
+    String result = '';
+    for (var genre in genres) {
+      result += '${genre.name}, ';
+    }
+
+    if (result.isEmpty) {
+      return result;
+    }
+
+    return result.substring(0, result.length - 2);
+  }
+
+  String _showDuration(num runtime) {
+    final num hours = runtime ~/ 60;
+    final num minutes = runtime % 60;
+
+    if (hours > 0) {
+      return '${hours}h ${minutes}m';
+    } else {
+      return '${minutes}m';
+    }
+  }
+
+}
+
+
